234c831852ff9c51309cc2aa34fcdd9ac5329e5a
jeffer <desarrollo@academicos.cesde.edu.co>
2024-06-12 17:38:47 -0500

    hicimos el controlador para acceder a la api de la BD

---
+++ controllers/controladorproducto.js
@@ -0,0 +1,19 @@
+//consumiendo el API para acceder a los datos de la BD
+
+let url="localhost:8080/storeapi/v1/producto"
+
+let peticion={
+    method:"GET",
+    headers:{}
+}
+
+fetch(url,peticion)
+.then(function(respuesta){
+    return respuesta.json()
+})
+.then(function(respuesta){
+    console.log(respuesta)
+})
+.catch(function(respuesta){
+    console.log(respuesta)
+})
